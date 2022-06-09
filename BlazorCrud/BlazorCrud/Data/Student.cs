4464c4a8cc67ab9638c88a6139642c386227d967
Sundram <sundramtpvc@gmail.com>
2022-06-09 10:36:10 +0530

    init

---
+++ BlazorCrud/BlazorCrud/Data/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Data
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int roll { get; set; }
+        public string Branch { get; set; }
+        public string College { get; set;}
+
+       
+    }
+}
